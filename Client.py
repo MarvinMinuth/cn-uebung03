5e0fff1152d1c16be5c4298b70dd156c85d84e12
Marv <marvin.minuth@gmail.com>
2022-05-05 13:37:27 +0200

    works as intended

--- Client.py
+++ Client.py
@@ -1,43 +1,21 @@
 import socket
-import threading
+import sys
 import os
 
 
 def client():
     c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     c_sock.connect(('127.0.0.1', 4242))
+    close = True
 
-    def receive():
-        while True:
-            message = c_sock.recv(255).decode('utf-8')
-            print(message)
-            if message.startswith("STATUS: OK"):
-                print('Close...')
-                write_thread.join()
-                print(write_thread.is_alive())
-                receive_thread.join()
-                print(receive_thread.is_alive())
-                c_sock.close()
-                exit(0)
-
-    def write():
-        while True:
-            message = f'{input("")}'
-            if message.startswith('PUT'):
-                put(message)
-            # elif message.startswith('GET'):
-            #     get()
-            else:
-                print('Options: "PUT {filename}" or "GET {filename}"')
-
-    def put(message):
-        # filename = message[4:]
-        filename = '2022S_CN04_Ethernet.pdf'
+    def put(command, close_command="close"):
+        # file_name = command[3:]
+        file_name = '2022S_CN04_Ethernet.pdf'
         try:
-            file_size = os.path.getsize(filename)
-            file = open(filename, 'rb')
+            file_size = os.path.getsize(file_name)
+            file = open(file_name, 'rb')
             print(f'Content-Length: {file_size}\n')
-            c_sock.send(f'PUT {filename}\n{file_size}\n\n'.encode('UTF-8'))
+            c_sock.send(f'PUT {file_name}\n{file_size}\n{close_command}\n\n'.encode('UTF-8'))
             data = file.read(1024)
             while data:
                 c_sock.send(data)
@@ -46,11 +24,55 @@ def client():
         except:
             print("File not found!")
 
-    receive_thread = threading.Thread(target=receive)
-    receive_thread.start()
+    def get(command, close_command="close"):
+        # filename = command[3:]
+        file_name = '2022S_CN04_Ethernet.pdf'
+        c_sock.send(f'GET {file_name}\n{close_command}\n'.encode('UTF-8'))
 
-    write_thread = threading.Thread(target=write)
-    write_thread.start()
+        answer = c_sock.recv(1024).decode('utf-8')
+        print(answer)
+        if answer.startswith('STATUS: FILE NOT FOUND'):
+            return
+
+        file_size = int(answer.split()[3])
+        file_path = './get/' + file_name
+        file = open(file_path, 'wb')
+        while file_size > 0:
+            if file_size >= 1024:
+                data = c_sock.recv(1024)
+                file_size -= 1024
+            else:
+                data = c_sock.recv(file_size)
+                file_size = 0
+            print(bin(int.from_bytes(data, byteorder=sys.byteorder)))
+            file.write(data)
+        file.close()
+
+    while True:
+        confirmation = c_sock.recv(255).decode('utf-8')
+        if confirmation.startswith('STATUS: OK'):
+            if close:
+                c_sock.close()
+                exit(0)
+        print(confirmation)
+
+        command = f'{input("")}'
+        if command.startswith('PUT'):
+            close_command = f'{input("Connection: ")}'
+            if close_command == "keep-alive":
+                close = False
+            else:
+                close = True
+            put(command, close_command)
+        elif command.startswith('GET'):
+            close_command = f'{input("Connection: ")}'
+            if close_command == "keep-alive":
+                close = False
+            else:
+                close = True
+            get(command, close_command)
+        else:
+            print("Usage: PUT [filename] or GET [filename]")
 
 
 def main():
--- Server.py
+++ Server.py
@@ -1,12 +1,15 @@
 # 1077466 - Marvin Minuth
 import socket
-import threading
+import sys
+import os
 
 
 def server():
     s_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_sock.bind(('127.0.0.1', 4242))
     s_sock.listen()
+    connected = False
+    close = False
 
     def client_put(message):
         file_name = message.split()[1]
@@ -20,28 +23,61 @@ def server():
             else:
                 data = c_sock.recv(file_size)
                 file_size = 0
-            print(data)
+            print(bin(int.from_bytes(data, byteorder=sys.byteorder)))
             file.write(data)
-            print('######################################################')
-
         file.close()
-        print("File closed")
         c_sock.send('STATUS: OK\n\n'.encode('UTF-8'))
-        c_sock.close()
-        print("Socket closed")
-        s_sock.close()
-        print("Server closed")
-        exit(0)
+
+    def client_get(message, close_command):
+        file_name = message.split()[1]
+        print(f"GET {file_name}\nConnection: {close_command}")
+        try:
+            file_size = os.path.getsize(file_name)
+            print(f"Content-Length: {file_size}")
+            file = open(file_name, 'rb')
+            c_sock.send(f'STATUS: OK\nContent-Length: {file_size}\n\n'.encode('UTF-8'))
+            data = file.read(1024)
+            while data:
+                c_sock.send(data)
+                data = file.read(1024)
+            file.close()
+
+        except:
+            c_sock.send("STATUS: FILE NOT FOUND".encode('utf-8'))
+            return
+
+        c_sock.send('STATUS: OK\n\n'.encode('UTF-8'))
+
 
     while True:
-        c_sock, c_addr = s_sock.accept()
-        print('Client connected')
-        c_sock.send("Connected to server.".encode('utf-8'))
+        if not connected:
+            c_sock, c_addr = s_sock.accept()
+            print('Client connected')
+            c_sock.send("Connected to server.".encode('utf-8'))
+            connected = True
+        if close:
+            c_sock.close()
+            s_sock.close()
+            exit(0)
         message = c_sock.recv(1024).decode('utf-8')
-        print(message)
         command = message[:3]
         if command == 'PUT':
+            try:
+                close_command = message.split()[3]
+            except:
+                close_command = "close"
+            if close_command != 'keep-alive':
+                close = True
             client_put(message)
+        elif command == 'GET':
+            try:
+                close_command = message.split('\n')[1]
+            except:
+                close_command = "close"
+            if close_command != 'keep-alive':
+                close = True
+            client_get(message, close_command)
+
 
 
 def main():
